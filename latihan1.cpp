878cc212741e84aa711fe48f74d463860d51c3b4
fawwaz-dzakwan <fawwazdzkwn@github.com>
2018-10-24 15:27:44 +0700

    latihan1

---
+++ latihan1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i=0;
+    int max=0;
+    int n,a;
+
+    cout << "masukan bilangan: ";
+    cin >> n;
+
+    for(i;i<n;i++) {
+        cout << "masukan bilangan ke" << i+1 << ":";
+        cin >> a;
+
+        if (a > max)
+            max = a;
+    }
+
+    cout << "bilangan terbesar adalah: " << max << endl;
+}
+
+
+
+
